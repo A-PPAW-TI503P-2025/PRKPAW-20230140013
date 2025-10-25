4b74af74d4ea100cae681566046d9b6c9de7509d
Mihaly24 <fairuzk20@gmail.com>
2025-10-25 10:18:46 +0700

    Update file praktikum

--- 013-node-server/my-node-server/controllers/reportController.js
+++ 013-node-server/my-node-server/controllers/reportController.js
@@ -1,8 +1,23 @@
-const presensiRecords = require("../data/presensiData");
-exports.getDailyReport = (req, res) => {
-  console.log("Controller: Mengambil data laporan harian dari array...");
-  res.json({
-    reportDate: new Date().toLocaleDateString(),
-    data: presensiRecords,
-  });
+const { Presensi } = require('../models');
+
+exports.getDailyReport = async (req, res) => {
+  try {
+    const today = new Date().toISOString().split('T')[0];
+    const records = await Presensi.findAll({
+      where: {
+        checkIn: {
+          [require('sequelize').Op.gte]: new Date(`${today}T00:00:00`),
+          [require('sequelize').Op.lt]: new Date(`${today}T23:59:59`)
+        }
+      }
+    });
+
+    res.json({
+      status: 'success',
+      reportDate: today,
+      data: records
+    });
+  } catch (error) {
+    res.status(500).json({ status: 'error', message: error.message });
+  }
 };
